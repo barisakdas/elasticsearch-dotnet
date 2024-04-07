901bbb4202826bdb54337fe875f853809605d06c
Barış Akdaş <barisakdas00@gmail.com>
2024-04-08 00:06:33 +0300

    controllers added

---
+++ Elasticsearch.Api/Controllers/AuthorController.cs
@@ -0,0 +1,85 @@
+﻿namespace Elasticsearch.Api.Controllers;
+
+/// <summary>Endpoinleri oluşturduğumuz yapı.</summary>
+[Produces("application/json")]
+[Route("v1/[controller]")]
+[ApiController]
+public class AuthorController : ControllerBase
+{
+    // İlgili servisi içeriye alıyoruz.
+    private readonly IAuthorService _service;
+
+    // Servisi DI container içerisinde dahil ediyoruz.
+    public AuthorController(IAuthorService service)
+       => _service = service;
+
+    [HttpGet("getall")] // Yapılan işlemin endpointinin nasıl görüneceğini burada `getall` diyerek belirtiyoruz.
+    public async Task<IActionResult> GetAllAsync()
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetAllAsync();
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpGet("getbyid")]
+    public async Task<IActionResult> GetByIdAsync(string id)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByIdAsync(id);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpGet("getbyfirstname")]
+    public async Task<IActionResult> GetByFirstNameAsync(string firstName)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByFirstNameAsync(firstName);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpPost("getbyfirstnamelist")]
+    public async Task<IActionResult> GetByFirstNameListAsync(List<string> firstNames)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByFirstNameListAsync(firstNames);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpPost("insert")]
+    public async Task<IActionResult> InsertAsync(CreateAuthorModel model)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.InsertAsync(model);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAsync(UpdateAuthorModel model)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.UpdateAsync(model);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteAsync(string id)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.DeleteAsync(id);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+}
---
+++ Elasticsearch.Api/Controllers/BookController.cs
@@ -0,0 +1,95 @@
+﻿namespace Elasticsearch.Api.Controllers;
+
+/// <summary>Endpoinleri oluşturduğumuz yapı.</summary>
+[Produces("application/json")]
+[Route("v1/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    // İlgili servisi içeriye alıyoruz.
+    private readonly IBookService _service;
+
+    // Servisi DI container içerisinde dahil ediyoruz.
+    public BookController(IBookService service)
+       => _service = service;
+
+    [HttpGet("getall")] // Yapılan işlemin endpointinin nasıl görüneceğini burada `getall` diyerek belirtiyoruz.
+    public async Task<IActionResult> GetAllAsync()
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetAllAsync();
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpGet("getbyid")]
+    public async Task<IActionResult> GetByIdAsync(string id)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByIdAsync(id);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpGet("getbyname")]
+    public async Task<IActionResult> GetByNameAsync(string name)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByNameAsync(name);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpPost("getbynamelist")]
+    public async Task<IActionResult> GetByNameListAsync(List<string> names)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByNameListAsync(names);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpGet("getbypublishdate")]
+    public async Task<IActionResult> GetByNameAsync(DateTime publishDate)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.GetByPublishDateAsync(publishDate);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpPost("insert")]
+    public async Task<IActionResult> InsertAsync(CreateBookModel model)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.InsertAsync(model);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAsync(UpdateBookModel model)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.UpdateAsync(model);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteAsync(string id)
+    {
+        // Servis üzerinden veriyi alıyoruz. Alınan bu veri bize Result<T> şeklinde döneceği için bunun yapılandırmasına ihtiyacımız var.
+        var result = await _service.DeleteAsync(id);
+
+        // Gelen verideki Result yapısının durumunu kontrol ederek ve ona uygun geri dönüş tipini (IActionResult) seçerek işlemi sonlandırıyoruz.
+        return this.FromResult(result);
+    }
+}
