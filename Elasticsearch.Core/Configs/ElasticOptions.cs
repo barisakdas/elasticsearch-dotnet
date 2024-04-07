bde9b3ae001cc944d1205135b2f85a8eac601452
Barış Akdaş <barisakdas00@gmail.com>
2024-04-08 00:03:32 +0300

    Elasticsearch configs added

---
+++ Elasticsearch.Core/Configs/ElasticOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Elasticsearch.Core.Configs;
+
+/// <summary>Elasticsearch üzerine bağlanmamızı sağlayacak ayarları barındıran sınıf.</summary>
+public class ElasticOptions
+{
+    /// <summary>Appsettings altında bulunan ve bilgilerin alındığı alan adı.</summary>
+    public string SectionName { get; } = "Elasticsearch";
+
+    /// <summary>Mevcutta kullanılan veri tabanının adresi.</summary>
+    public string Url { get; set; } = null!;
+
+    /// <summary>Bağlantı için gerekli kullanıcı adı.</summary>
+    public string Client { get; set; } = null!;
+
+    /// <summary>Bağlantı için gerekli kullanıcı parola.</summary>
+    public string Secret { get; set; } = null!;
+}
