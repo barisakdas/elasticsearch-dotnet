ab29760436305f84c3ef2be6829bb1245387209d
Barış Akdaş <barisakdas00@gmail.com>
2024-04-08 00:04:35 +0300

    mappers added

---
+++ Elasticsearch.Core/Mappers/AuthorProfile.cs
@@ -0,0 +1,24 @@
+﻿namespace Elasticsearch.Core.Mappers;
+
+/// <summary>Profile sınıfı oluşturulan classlar arasında eşleme işlemi yapar.</summary>
+public class AuthorProfile : AutoMapper.Profile   // AutoMapper dan gelen Profile sınıfını miras alıyoruz.
+{
+    // Yapıcı metot içerisinde hangi sınıflardan hangi sınıflara eşleme işlemi yapacağımızı belirtiyoruz.
+    public AuthorProfile()
+    {
+        // Author entity'si ile AuthorDto geri dönüş modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<Author, AuthorDto>().ReverseMap();
+
+        // Author entity'si ile CreateAuthorModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<CreateAuthorModel, Author>().ReverseMap();
+
+        // AuthorDto geri dönüş modeli ile CreateAuthorModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<CreateAuthorModel, AuthorDto>().ReverseMap();
+
+        // Author entity'si ile CreateAuthorModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<UpdateAuthorModel, Author>().ReverseMap();
+
+        // AuthorDto geri dönüş modeli ile CreateAuthorModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<UpdateAuthorModel, AuthorDto>().ReverseMap();
+    }
+}
---
+++ Elasticsearch.Core/Mappers/BookProfile.cs
@@ -0,0 +1,24 @@
+﻿namespace Elasticsearch.Core.Mappers;
+
+/// <summary>Profile sınıfı oluşturulan classlar arasında eşleme işlemi yapar.</summary>
+public class BookProfile : AutoMapper.Profile   // AutoMapper dan gelen Profile sınıfını miras alıyoruz.
+{
+    // Yapıcı metot içerisinde hangi sınıflardan hangi sınıflara eşleme işlemi yapacağımızı belirtiyoruz.
+    public BookProfile()
+    {
+        // Book entity'si ile BookDto geri dönüş modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<Book, BookDto>().ReverseMap();
+
+        // Book entity'si ile CreateBookModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<CreateBookModel, Book>().ReverseMap();
+
+        // BookDto geri dönüş modeli ile CreateBookModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<CreateBookModel, BookDto>().ReverseMap();
+
+        // Book entity'si ile CreateBookModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<UpdateBookModel, Book>().ReverseMap();
+
+        // BookDto geri dönüş modeli ile CreateBookModel oluşturma modeli arasında ve tam tersi yöndede olmak üzere bir eşleme yapıyoruz.
+        CreateMap<UpdateBookModel, BookDto>().ReverseMap();
+    }
+}
